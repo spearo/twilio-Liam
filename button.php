155bdda9b79f02284cf2c9be0355b227622bcaba
spearo <liam.richards@gmail.com>
2013-03-22 15:49:02 +0000

    funny

--- button.php
+++ button.php
@@ -3,7 +3,7 @@
   if($button_pressed == 1){
     $response = "You pressed button 1";
   }else{
-    $response = "haha, you is a wanker!! $button_pressed";
+    $response = "haha, clown - you just credited Liam with £1!! $button_pressed";
   }
   header("content-type: text/xml");
   echo "<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n";
